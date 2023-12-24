340b2c7adcbb2a9a083c7407a6fe0a29574eacb9
ABHINAV <abhinavpatel0208@gmail.com>
2023-12-24 19:02:03 +0530

    Use IActionMethod

--- WebApiDemo/Controllers/ShirtsController.cs
+++ WebApiDemo/Controllers/ShirtsController.cs
@@ -7,40 +7,56 @@ namespace WebApiDemo.Controllers
     [Route("api/[controller]")] // Now We Dont need to mention in each methodx
     public class ShirtsController : ControllerBase
     {
+
+        private List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt{ShirtId = 1 , Brand="Nike", Color="Pink", Gender="Men" , Price= 90 , Size=10},
+            new Shirt{ShirtId = 2 , Brand="Adidas", Color="black", Gender="Men" , Price= 960 , Size=17},
+            new Shirt{ShirtId = 3 , Brand="Nike", Color="Pink", Gender="Women" , Price= 907 , Size=8},
+            new Shirt{ShirtId = 4 , Brand="PinkVilla", Color="Purple", Gender="Women" , Price= 906 , Size=9},
+
+        };
+
         [HttpGet]
         //[Route("/shirts")]
-        public string GetShirts() 
+        public IActionResult GetShirts() 
         {
-            return "Reading all the shirts";
+            return Ok("Reading all the shirts");
         }
 
         [HttpGet("{id}")]
         //[Route("/shirts/{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id)
         {
-            return $"Reading Shirt :{id}";
+            if (id < 0)
+                return BadRequest();
+
+            var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+            if (shirt == null)
+                return NotFound();
+            return Ok(shirt);
         }
 
         [HttpPost]
         //[Route("/shirts")]
-        public string CreateShirt([FromBody]Shirt shirt) //JSon
-        //public string CreateShirt([FromForm] Shirt shirt) //Body Key-value
+        public IActionResult CreateShirt([FromBody]Shirt shirt) //JSon
+        //public IActionResult CreateShirt([FromForm] Shirt shirt) //Body Key-value
         {
-            return "creatig a Shirt";
+            return Ok("creatig a Shirt");
         }
 
         [HttpPut("{id}")]
         //[Route("/shirts/{id}")]
-        public string UpdateShirt(int id) 
+        public IActionResult UpdateShirt(int id) 
         {
-            return $"Updating Shirt :{id}";
+            return Ok($"Updating Shirt :{id}");
         }
 
         [HttpDelete("{id}")]
         //[Route("/shirts/{id}")]
-        public string DeleteShirt(int id) 
+        public IActionResult DeleteShirt(int id) 
         {
-            return $"Deleting Shirt :{id}";
+            return Ok($"Deleting Shirt :{id}");
         }
     }
 }
