3e6c86e1792153543ad3e4a5e86ad911b3dc8938
ABHINAV <abhinavpatel0208@gmail.com>
2023-12-24 12:24:36 +0530

    Update Routing

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -8,7 +8,34 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-//Routing 
-//
+//Routing For Web API
+// "/shirts"
+//Read All Shirt
+app.MapGet("/shirts",()=>
+{
+    return "Reading all the shirts";
+});
+
+//Read a shirt using Id
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"Reading Shirt with ID:{id}";
+});
+
+//Create a shirt
+app.MapPost("/shirts", () =>
+{
+    return "Creating a shirt";
+});
+
+//Update the shirt
+app.MapPut("/shirts/{id}", (int id) => {
+    return $"Updating shirt with Id: {id}";
+});
+
+//Delete the shirt
+app.MapDelete("/shirts/{id}", (int id) => {
+    return $"Deleting Shirt with Id : {id}";
+});
 
 app.Run();
